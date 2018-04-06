d323faf12583f6f7ea7ea83e236151ff9d997532
yinzm <yinzongming@gmail.com>
2018-04-06 14:12:08 +0800

    add better solution

--- Array/easy/122_Best Time to Buy and Sell Stock II.cpp
+++ Array/easy/122_Best Time to Buy and Sell Stock II.cpp
@@ -8,7 +8,7 @@ public:
         if (len == 0) {
             return 0;
         }
-        int dp[len+1][2];
+        int dp[len+1][2]; // 第一维代表第几个价格，第二维代表是否手头有该物品，0代表没有物品，1代表有。
         dp[1][0] = 0;
         dp[1][1] = -prices[0];
         for (int i = 2; i <= len; ++i) {
@@ -16,6 +16,21 @@ public:
             dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i-1]);
         }
         return max(dp[len][0], dp[len][1]);
+    }
+    // greed method, official solution
+    // link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solution/
+    int maxProfit2(vector<int>& prices) {
+        int len = prices.size();
+        if (len <= 1) {
+            return 0;
+        }
+        int maxP = 0;
+        for (int i = 1; i < len; ++i) {
+            if (prices[i] > prices[i-1]) {
+                maxP += (prices[i] - prices[i-1]);
+            }
+        }
+        return maxP;
     }
 };
 
@@ -28,6 +43,6 @@ int main() {
         cin >> x;
         prices.push_back(x);
     }
-    cout << s.maxProfit(prices) << endl;
+    cout << s.maxProfit2(prices) << endl;
     return 0;
 }
